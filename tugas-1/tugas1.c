aae135f60bc53fd63a596d47f67238a258c586b4
marshaagustin <marshaagustin2005@gmail.com>
2023-11-09 17:56:35 +0700

    first commit

---
+++ tugas-1/tugas1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char nim[10] = "2341076";
+  char nama[30] = "Marsha Agustin";
+ char jurusan[30] = "Informatika";
+
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
---
+++ tugas-1/tugas2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main( )
+{ 
+      int panjang = 10 ; 
+      int lebar = 7 ; 
+      int hasil = panjang*lebar ; 
+      
+      printf ( "rumus  :  P  ×  L   \n ") ;
+      printf ( "maka  %d cm × %d cm \n") , panjang , lebar ) ; 
+      printf ( " hasil = %d cm" , hasil ) ; 
+      
+      return 0 ; 
+}
